a0b86481200440df81041d57e66a6eb7911dc16f
Oğuzhan Ercan <oguzhanercn7@gmail.com>
2019-09-01 15:28:40 +0300

    C# İle Wifi Kontrol Yapma

---
+++ İnternetKontrolu/İnternetKontrolu/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace İnternetKontrolu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.PicDurum = new System.Windows.Forms.PictureBox();
+            this.LblSonuc = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDurum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(79, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAĞLANTI KONTROLÜ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PicDurum
+            // 
+            this.PicDurum.Location = new System.Drawing.Point(99, 118);
+            this.PicDurum.Name = "PicDurum";
+            this.PicDurum.Size = new System.Drawing.Size(116, 100);
+            this.PicDurum.TabIndex = 1;
+            this.PicDurum.TabStop = false;
+            // 
+            // LblSonuc
+            // 
+            this.LblSonuc.AutoSize = true;
+            this.LblSonuc.Location = new System.Drawing.Point(136, 245);
+            this.LblSonuc.Name = "LblSonuc";
+            this.LblSonuc.Size = new System.Drawing.Size(0, 13);
+            this.LblSonuc.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(293, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(326, 380);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.LblSonuc);
+            this.Controls.Add(this.PicDurum);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PicDurum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox PicDurum;
+        private System.Windows.Forms.Label LblSonuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ İnternetKontrolu/İnternetKontrolu/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace İnternetKontrolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ping ping = new Ping();
+            PingReply pingstatü = ping.Send(IPAddress.Parse("208.69.34.231"));
+
+            if (pingstatü.Status == IPStatus.Success)
+            {
+                PicDurum.BackgroundImage = Image.FromFile(Application.StartupPath + "\\wifi.jpg");
+                LblSonuc.Text = "Bağlantı Var";
+            }
+
+            else
+            {
+                PicDurum.BackgroundImage = Image.FromFile("NoWifi.jpg");
+                LblSonuc.Text = "Bağlantı Yok";
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
